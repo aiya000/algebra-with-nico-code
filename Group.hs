33974380d2fc574c2fe941c01e90c17413adcbaa
aiya000 <aiya000.develop@gmail.com>
2018-08-13 23:08:59 +0900

    :+1: Make Group.hs

---
+++ Group.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Group
+  ( Group (..)
+  ) where
+
+import Monoid
+import Prelude hiding (Semigroup(..), Monoid(..))
+import Semigroup
+import Test.SmallCheck (smallCheck)
+
+class Monoid a => Group a where
+  inverse :: a -> a
+
+instance Group SumInt where
+  inverse = negate
+
+instance Group () where
+  inverse _ = ()
+
+aSumInt :: SumInt
+aSumInt = inverse empty
+
+aUnit :: ()
+aUnit = inverse empty
+
+inverseLaw :: (Group a, Eq a) => a -> Bool
+inverseLaw x =
+  x <> inverse x == empty && empty == inverse x <> x
+
+main :: IO ()
+main = do
+  smallCheck 2 $ inverseLaw @ SumInt
+  smallCheck 2 $ inverseLaw @ ()

2349edff3b1021d3b6becffe5b10852de4569c80
aiya000 <aiya000.develop@gmail.com>
2018-08-16 22:22:53 +0900

    :recycle: Remove unnecessary data
    I think `Num` examples are enough with Int,
      but thanks Float.

--- Core.hs
+++ Core.hs
@@ -10,21 +10,13 @@ module Core where
 import Test.SmallCheck.Series (Serial(..), Series)
 
 -- #@@range_begin(wrapper_types)
-newtype SumInt = SumInt
-  { unSumInt :: Int
+newtype Sum = Sum
+  { unSum :: Int
   } deriving (Show, Eq, Num, Enum)
 
-newtype ProductInt = ProductInt
-  { unProductInt :: Int
+newtype Product = Product
+  { unProduct :: Int
   } deriving (Show, Eq, Num, Enum)
-
-newtype SumFloat = SumFloat
-  { unSumFloat :: Float
-  } deriving (Show, Eq, Num)
-
-newtype ProductFloat = ProductFloat
-  { unProductFloat :: Float
-  } deriving (Show, Eq, Num)
 
 newtype And = And
   { unAnd :: Bool
@@ -35,21 +27,13 @@ newtype Or = Or
   } deriving (Show, Eq)
 -- #@@range_end(wrapper_types)
 
-instance Monad m => Serial m SumInt where
-  series :: Series m SumInt
-  series = SumInt <$> series
+instance Monad m => Serial m Sum where
+  series :: Series m Sum
+  series = Sum <$> series
 
-instance Monad m => Serial m ProductInt where
-  series :: Series m ProductInt
-  series = ProductInt <$> series
-
-instance Monad m => Serial m SumFloat where
-  series :: Series m SumFloat
-  series = SumFloat <$> series
-
-instance Monad m => Serial m ProductFloat where
-  series :: Series m ProductFloat
-  series = ProductFloat <$> series
+instance Monad m => Serial m Product where
+  series :: Series m Product
+  series = Product <$> series
 
 instance Monad m => Serial m And where
   series :: Series m And
--- Semigroup.hs
+++ Semigroup.hs
@@ -2,8 +2,8 @@
 
 module Semigroup
   ( Semigroup (..)
-  , SumInt (..)
-  , ProductInt (..)
+  , Sum (..)
+  , Product (..)
   , And (..)
   , Or (..)
   ) where
@@ -23,9 +23,6 @@ instance Semigroup Int where
   (<>) = (+)
 -- #@@range_end(int_instance)
 
-instance Semigroup Float where
-  (<>) = (+)
-
 instance Semigroup [a] where
   (<>) = (++)
 
@@ -37,16 +34,10 @@ instance Semigroup () where
 -- #@@range_end(basic_instances)
 
 -- #@@range_begin(newtype_instances)
-instance Semigroup SumInt where
+instance Semigroup Sum where
   (<>) = (+)
 
-instance Semigroup ProductInt where
-  (<>) = (*)
-
-instance Semigroup SumFloat where
-  (<>) = (+)
-
-instance Semigroup ProductFloat where
+instance Semigroup Product where
   (<>) = (*)
 
 instance Semigroup And where
@@ -57,11 +48,11 @@ instance Semigroup Or where
 -- #@@range_end(newtype_instances)
 
 -- #@@range_begin(int_instances_examples)
-aSumInt :: SumInt
-aSumInt = SumInt 10 <> SumInt 20
+aSumInt :: Sum
+aSumInt = Sum 10 <> Sum 20
 
-aProductInt :: ProductInt
-aProductInt = ProductInt 10 <> ProductInt 20
+aProductInt :: Product
+aProductInt = Product 10 <> Product 20
 -- #@@range_end(int_instances_examples)
 
 -- #@@range_begin(law)
@@ -76,10 +67,10 @@ assocLaw x y z =
 concat :: Semigroup a => a -> [a] -> a
 concat initial xs = foldl (<>) initial xs
 
-result :: SumInt
+result :: Sum
 result = sum [1..100]
   where
-    sum :: [SumInt] -> SumInt
+    sum :: [Sum] -> Sum
     sum [] = 0
     sum xs = concat 0 xs
 -- #@@range_end(practice)
@@ -87,10 +78,8 @@ result = sum [1..100]
 -- #@@range_begin(tests_for_law)
 main :: IO ()
 main = do
-  smallCheck 2 $ assocLaw @ SumInt
-  smallCheck 2 $ assocLaw @ ProductInt
-  smallCheck 2 $ assocLaw @ SumFloat
-  smallCheck 2 $ assocLaw @ ProductFloat
+  smallCheck 2 $ assocLaw @ Sum
+  smallCheck 2 $ assocLaw @ Product
   smallCheck 2 $ assocLaw @ [Double]
   smallCheck 2 $ assocLaw @ And
   smallCheck 2 $ assocLaw @ Or

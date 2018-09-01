b5e79394852bf32aac18cd12bd0dcbe2a62fa0bb
aiya000 <aiya000.develop@gmail.com>
2018-09-01 23:24:28 +0900

    :+1: Add ranges to Ring

--- Ring.hs
+++ Ring.hs
@@ -10,12 +10,18 @@ import Data.Ratio ((%))
 import Prelude hiding ((<>))
 import Test.SmallCheck (smallCheck)
 
+-- #@@range_begin(class)
 class Ring a where
   (<>)     :: a -> a -> a
   emptyA   :: a
   inverseA :: a -> a
   (><)     :: a -> a -> a
 
+infixl 6 <>
+infixl 7 ><
+-- #@@range_end(class)
+
+-- #@@range_begin(instances)
 instance Ring Int where
   (<>)     = (+)
   emptyA   = 0
@@ -33,10 +39,9 @@ instance Ring () where
   emptyA      = ()
   inverseA () = ()
   () >< ()    = ()
+-- #@@range_end(instances)
 
-infixl 6 <>
-infixl 7 ><
-
+-- #@@range_begin(usual_laws)
 associativeLawForAdd :: (Ring a, Eq a) => a -> a -> a -> Bool
 associativeLawForAdd x y z =
   (x <> y) <> z == x <> (y <> z)
@@ -49,14 +54,18 @@ inverseLawForAdd :: (Ring a, Eq a) => a -> Bool
 inverseLawForAdd x =
   (x <> inverseA x == emptyA) && (emptyA == inverseA x <> x)
 
-distributiveLaw :: (Ring a, Eq a) => a -> a -> a -> Bool
-distributiveLaw x y z =
-  x >< (y <> z) == x >< y <> x >< z &&
-  (y <> z) >< x == y >< x <> z >< x
-
 associativeLawForMulti :: (Ring a, Eq a) => a -> a -> a -> Bool
 associativeLawForMulti x y z =
   (x >< y) >< z == x >< (y >< z)
+-- #@@range_end(usual_laws)
+
+-- #@@range_begin(newer_law)
+distributiveLaw :: (Ring a, Eq a) => a -> a -> a -> Bool
+distributiveLaw x y z =
+  x >< (y <> z) == x >< y <> x >< z
+    &&
+  (y <> z) >< x == y >< x <> z >< x
+-- #@@range_end(newer_law)
 
 checkAssociativeLawForAdd :: IO ()
 checkAssociativeLawForAdd = do
